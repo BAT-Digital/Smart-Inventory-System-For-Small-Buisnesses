3dfeb3e7c65451d866009e3cd017e59301027bf7
Ali Tulegenov <220264@astanait.edu.kz>
2025-02-06 16:45:14 +0500

    Inventory batch fixed

--- src/main/java/com/example/SmartInventorySystem/model/InventoryBatch.java
+++ src/main/java/com/example/SmartInventorySystem/model/InventoryBatch.java
@@ -2,31 +2,38 @@ package com.example.SmartInventorySystem.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
-import java.util.Date;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 @Table(name = "inventory_batch")
 public class InventoryBatch {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "batch_id")
     private Long batchId;
 
-    @Column(name = "product_id",nullable = false)
-    private Long productID;
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
 
-    @Column(name = "quantity",nullable = false)
+    @Column(name = "quantity", nullable = false)
     private Integer quantity;
 
-    @Column(name = "unit_cost",nullable = false)
-    private Integer unitCost;
+    @Column(name = "unit_cost", nullable = false, precision = 10, scale = 2)
+    private BigDecimal unitCost;
 
-    @Column(name = "expiration_date",nullable = false)
-    private Date expirationDate;
+    @Column(name = "expiration_date")
+    private LocalDate expirationDate;
 
-    @Column(name = "received_date",nullable = false)
-    private Date receivedDate;
+    @CreationTimestamp
+    @Column(name = "received_date", updatable = false)
+    private LocalDateTime receivedDate;
 
-    @Column(name = "status",nullable = false)
-    private String status;
+    @Column(name = "status")
+    private String status = "active";
 }
